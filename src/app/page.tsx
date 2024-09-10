3ceae4dc103477890043b44ceb987a0777dc75de
Payman <payman.hoseini7@gmail.com>
2024-09-11 00:33:28 +0430

    the responsive is hard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,53 @@
 import Image from "next/image";
+import Header from "./components/header";
+import eggDesktop from './../../public/images/desktop/image-transform.jpg'
+import glassDesktop from './../../public/images/desktop/image-stand-out.jpg'
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="w-screen h-screen">
+        <Header />
+        <div className="flex">
+          <div className="py-32 px-32 basis-1/2">
+            <h2 className="font-Fraunces font-extrabold text-5xl text-Very-dark-desaturated-blue">Transform your brand</h2>
+            <p className="my-9 font-Barlow text-Dark-grayish-blue"> We are a full-service creative agency specializing in helping brands grow fast. 
+            Engage your clients through compelling visuals that do most of the marketing for you.</p>
+            <button className="font-Fraunces font-extrabold text-Very-dark-desaturated-blue">LEARN MORE</button>
+          </div>
+          <div>
+            <Image src={eggDesktop} alt="Eggs"/>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex">
+          <div>
+            <Image src={glassDesktop} alt="glass"/>
+          </div>
+          <div className="py-32 px-28 basis-1/2">
+            <h2 className="font-Fraunces font-extrabold text-5xl text-Very-dark-desaturated-blue">Stand out to the right audience</h2>
+            <p className="my-9 font-Barlow text-Dark-grayish-blue"> Using a collaborative formula of designers, 
+              researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.</p>
+            <button className="font-Fraunces font-extrabold text-Very-dark-desaturated-blue">LEARN MORE</button>
+          </div>
+        </div>
+        <div className="flex w-full h-full">
+          <div className="gBack basis-1/2">
+            <div className="grid place-items-center place-content-center h-full px-40 mt-28">
+              <h2 className="font-Fraunces text-Dark-desaturated-cyan font-extrabold text-2xl">Graphic Design</h2>
+              <p className="font-Barlow text-Dark-desaturated-cyan mt-5 text-center"> Great design makes you memorable. We deliver artwork that underscores your brand message and 
+                captures potential clients’ attention.</p>
+            </div>
+          </div>
+          <div className="oBack basis-1/2">
+            <div className="grid place-items-center place-content-center h-full px-40 mt-28">
+              <h2 className="font-Fraunces text-Dark-blue font-extrabold text-2xl">Photography</h2>
+              <p className="font-Barlow text-Dark-desaturated-cyan mt-5 text-center">Increase your credibility by getting the most stunning, high-quality photos that 
+                improve your business image.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
